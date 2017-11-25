aacc02458d950eadb2fa252763b33cefe66f4448
steventranjs <tranquangtrung1989@gmail.com>
2017-11-26 01:08:28 +0800

    promise create acc

--- src/layouts/CreateFakeData.js
+++ src/layouts/CreateFakeData.js
@@ -18,18 +18,20 @@ const createFakeData = async () => {
       infiniteContract.setProvider(web3.currentProvider)
       const contractInstance = await infiniteContract.deployed()
 
-      await contractInstance.signup('Weup', ZERO_ACCOUNT, true, 1, 'WEUP', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Alice', user1, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Amazone', merchant1, true, 2, 'AMAZONE', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Bob', user2, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Ebay', merchant2, true, 3, 'EBAY', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Michael', user3, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Rakuten', merchant3, true, 4, 'RACKUTEN', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Lynn', user4, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Alibaba', merchant4, true, 5, 'ALIBABA', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Jack', user5, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 })
-      await contractInstance.signup('Lazada', merchant5, true, 6, 'LAZADA', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 })
-
+      console.log('Creating new acc...')
+      await Promise.all([
+          contractInstance.signup('Weup', ZERO_ACCOUNT, true, 1, 'WEUP', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Alice', user1, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Amazone', merchant1, true, 2, 'AMAZONE', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Bob', user2, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Ebay', merchant2, true, 3, 'EBAY', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Michael', user3, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Rakuten', merchant3, true, 4, 'RACKUTEN', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Lynn', user4, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Alibaba', merchant4, true, 5, 'ALIBABA', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Jack', user5, false, 0, 'null', 'null', { from: ZERO_ACCOUNT, gas: 1000000 }),
+          contractInstance.signup('Lazada', merchant5, true, 6, 'LAZADA', 'https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png', { from: ZERO_ACCOUNT, gas: 1000000 })
+      ])
       console.log('Weup: ', ZERO_ACCOUNT)
       console.log('User 1: ', user1)
       console.log('Merchant 1: ', merchant1)
@@ -42,29 +44,38 @@ const createFakeData = async () => {
       console.log('User 5: ', user5)
       console.log('Merchant 5: ', merchant5)
 
-      await contractInstance.addPoints(user1, 1000, { from: merchant1 })
-      await contractInstance.addPoints(user2, 2000, { from: merchant1 })
-      await contractInstance.addPoints(user1, 3000, { from: merchant2 })
-      await contractInstance.addPoints(user2, 4000, { from: merchant2 })
-      await contractInstance.addPoints(user1, 3000, { from: merchant3 })
-      await contractInstance.addPoints(user2, 4000, { from: merchant3 })
-      await contractInstance.addPoints(user1, 3000, { from: merchant4 })
-      await contractInstance.addPoints(user2, 4000, { from: merchant4 })
-      await contractInstance.addPoints(user1, 3000, { from: merchant5 })
-      await contractInstance.addPoints(user2, 4000, { from: merchant5 })
+      console.log('Adding points...')
+      await Promise.all([
+          contractInstance.addPoints(user1, 1000, { from: merchant1 }),
+          contractInstance.addPoints(user2, 2000, { from: merchant1 }),
+          contractInstance.addPoints(user1, 3000, { from: merchant2 }),
+          contractInstance.addPoints(user2, 4000, { from: merchant2 }),
+          contractInstance.addPoints(user1, 3000, { from: merchant3 }),
+          contractInstance.addPoints(user2, 4000, { from: merchant3 }),
+          contractInstance.addPoints(user1, 3000, { from: merchant4 }),
+          contractInstance.addPoints(user2, 4000, { from: merchant4 }),
+          contractInstance.addPoints(user1, 3000, { from: merchant5 }),
+          contractInstance.addPoints(user2, 4000, { from: merchant5 })
+      ])
 
-      await contractInstance.addWcoins(user1, 1000, { from: ZERO_ACCOUNT })
-      await contractInstance.addWcoins(user2, 2000, { from: ZERO_ACCOUNT })
-      await contractInstance.addWcoins(user3, 3000, { from: ZERO_ACCOUNT })
-      await contractInstance.addWcoins(user4, 4000, { from: ZERO_ACCOUNT })
-      await contractInstance.addWcoins(user5, 4000, { from: ZERO_ACCOUNT })
+      console.log('Adding wcoins...')
+      await Promise.all([
+          contractInstance.addWcoins(user1, 1000, { from: ZERO_ACCOUNT }),
+          contractInstance.addWcoins(user2, 2000, { from: ZERO_ACCOUNT }),
+          contractInstance.addWcoins(user3, 3000, { from: ZERO_ACCOUNT }),
+          contractInstance.addWcoins(user4, 4000, { from: ZERO_ACCOUNT }),
+          contractInstance.addWcoins(user5, 4000, { from: ZERO_ACCOUNT })
+      ])
 
-      await contractInstance.createOffer('usr3Buy', 'buy', merchant1, merchant2, 30, { from: user3, gas: 1000000 })
-      await contractInstance.createOffer('usr3Sell', 'sell', merchant2, merchant3, 40, { from: user3, gas: 1000000 })
-      await contractInstance.createOffer('usr4Buy', 'buy', merchant3, merchant4, 50, { from: user4, gas: 1000000 })
-      await contractInstance.createOffer('usr4Sell', 'sell', merchant4, merchant5, 60, { from: user4, gas: 1000000 })
-      await contractInstance.createOffer('usr5Buy', 'buy', merchant5, merchant4, 70, { from: user5, gas: 1000000 })
-      await contractInstance.createOffer('usr5Sell', 'sell', merchant1, merchant5, 80, { from: user5, gas: 1000000 })
+      console.log('Create Offer...')
+      await Promise.all([
+          contractInstance.createOffer('usr3Buy', 'buy', merchant1, merchant2, 30, { from: user3, gas: 1000000 }),
+          contractInstance.createOffer('usr3Sell', 'sell', merchant2, merchant3, 40, { from: user3, gas: 1000000 }),
+          contractInstance.createOffer('usr4Buy', 'buy', merchant3, merchant4, 50, { from: user4, gas: 1000000 }),
+          contractInstance.createOffer('usr4Sell', 'sell', merchant4, merchant5, 60, { from: user4, gas: 1000000 }),
+          contractInstance.createOffer('usr5Buy', 'buy', merchant5, merchant4, 70, { from: user5, gas: 1000000 }),
+          contractInstance.createOffer('usr5Sell', 'sell', merchant1, merchant5, 80, { from: user5, gas: 1000000 })
+      ])
 
       console.log('SEED DATA SUCCESS!!!!')
   } catch (err) {
