5c7aeff6de8552f34d65e4a8bca9c3f0c0ebbe39
steventranjs <tranquangtrung1989@gmail.com>
2017-11-25 15:04:07 +0800

    fix: create addPoints

--- src/layouts/CreateFakeData.js
+++ src/layouts/CreateFakeData.js
@@ -16,18 +16,24 @@ const createFakeData = async () => {
       infiniteContract.setProvider(web3.currentProvider)
       const contractInstance = await infiniteContract.deployed()
 
-      await contractInstance.signup('Weup', accounts[0], true, 1, { from: ZERO_ACCOUNT })
-      await contractInstance.signup('User 1', accounts[1], false, 0, { from: ZERO_ACCOUNT })
-      await contractInstance.signup('Merchant 1', accounts[2], true, 2, { from: ZERO_ACCOUNT })
+      const [weUp, user1, merchant1, user2, merchant2] = [accounts[0], accounts[1], accounts[2], accounts[3], accounts[4]]
 
-      await contractInstance.signup('User 2', accounts[3], false, 0, { from: ZERO_ACCOUNT })
-      await contractInstance.signup('Merchant 2', accounts[4], true, 2, { from: ZERO_ACCOUNT })
+      await contractInstance.signup('Weup', weUp, true, 1, { from: ZERO_ACCOUNT })
+      await contractInstance.signup('User 1', user1, false, 0, { from: ZERO_ACCOUNT })
+      await contractInstance.signup('Merchant 1', merchant1, true, 2, { from: ZERO_ACCOUNT })
 
-      console.log('Weup: ', accounts[0])
-      console.log('User 1: ', accounts[1])
-      console.log('Merchant 1: ', accounts[2])
-      console.log('User 2: ', accounts[3])
-      console.log('Merchant 2: ', accounts[4])
+      await contractInstance.signup('User 2', user2, false, 0, { from: ZERO_ACCOUNT })
+      await contractInstance.signup('Merchant 2', merchant2, true, 2, { from: ZERO_ACCOUNT })
+
+      console.log('Weup: ', weUp)
+      console.log('User 1: ', user1)
+      console.log('Merchant 1: ', merchant1)
+      console.log('User 2: ', user2)
+      console.log('Merchant 2: ', merchant2)
+      await contractInstance.addPoints(user1, 100, { from: merchant1 })
+      await contractInstance.addPoints(user2, 200, { from: merchant1 })
+      await contractInstance.addPoints(user1, 300, { from: merchant2 })
+      await contractInstance.addPoints(user2, 400, { from: merchant2 })
   } catch (err) {
     console.log('ERROR!!!:', err)
   }
