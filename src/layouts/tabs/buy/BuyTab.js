e88ab561d62264855427416f4be50631edc1193a
steventranjs <tranquangtrung1989@gmail.com>
2017-11-25 19:24:51 +0800

    fix: buys display

--- src/layouts/tabs/buy/BuyTab.js
+++ src/layouts/tabs/buy/BuyTab.js
@@ -43,7 +43,7 @@ class BuyTab extends Component {
                 wrap
               >{i.username}<br/>
                 <span className="item-buy-amount">buys <strong>{i.buy_amount} {i.merchant_code} pts.</strong></span><br/>
-                <span className="item-buy-price">Price: <strong>{i.buyt_total_price} WeCoin</strong></span><br/>
+                <span className="item-buy-price">Price: <strong>{i.buy_total_price} WeCoin</strong></span><br/>
               </Item>
             ))}
           </List>
