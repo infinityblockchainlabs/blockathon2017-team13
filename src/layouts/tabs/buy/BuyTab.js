88e3026e93ae6ac1c39c8d67982c94c8d298161a
Vinh Nguyen <vinhngle@gmail.com>
2017-11-26 02:17:46 +0700

    update buy/sell UI

--- src/layouts/tabs/buy/BuyTab.js
+++ src/layouts/tabs/buy/BuyTab.js
@@ -31,17 +31,18 @@ class BuyTab extends Component {
         <WingBlank>
           <WhiteSpace />
           <div className="wecoin">
-            <span>{this.props.user.wecoinBalance}</span> WeCoin</div>
+            <span>{this.props.user.wecoinBalance}</span> WeCoin
+          </div>
           <WhiteSpace />
           {buyList && buyList.length > 0 && <List>
             {buyList.map(i => (
               <Item style={{fontSize: 14}}
-                key={i.id}
+                    key={i.id}
                 // thumb={i.merchant_icon}
-                extra={<Button type="primary" style={{width: 70, float: 'right'}}>Buy</Button>}
-                onClick={() => this.props.buyOffer(i.id)}
-                multipleLine
-                wrap>
+                    extra={<Button type="primary" style={{width: 70, float: 'right'}}>Buy</Button>}
+                    onClick={() => this.props.buyOffer(i.id)}
+                    multipleLine
+                    wrap>
                 <span style={{fontSize: 12, color: 'gray'}}><strong>{i.username}</strong> wants to sell</span><br/>
                 <span className="item-buy-amount"><strong>{i.buy_amount} {i.merchant_code}</strong> points</span><br/>
                 <span className="item-buy-price">for <strong>{i.buy_total_price} WeCoin</strong></span>
@@ -50,9 +51,9 @@ class BuyTab extends Component {
           </List>
           }
         </WingBlank>
-        </div>
-        )
-        }
-        }
+      </div>
+    )
+  }
+}
 
-        export default BuyTab
+export default BuyTab
